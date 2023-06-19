aece26562400f66dd6bed4ff14a59c319dc6481b
Simon <47429862+LautrecSec@users.noreply.github.com>
2023-06-19 17:29:09 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Import necessary modules
 import os
 import sys
 from PyQt5.QtCore import QThread, pyqtSignal
@@ -5,40 +6,51 @@ from PyQt5.QtWidgets import QApplication, QFileDialog, QLabel, QLineEdit, QPushB
 from pytube import YouTube, exceptions
 from moviepy.editor import AudioFileClip
 
+# The DownloadThread class is used to create a separate thread for downloading the video
 class DownloadThread(QThread):
     progress_signal = pyqtSignal(int)
 
-    def __init__(self, url, folder):
+    def __init__(self, url, folder, stream_type):
         super().__init__()
-        self.url = url.strip()  # Remove potential leading/trailing whitespaces
-        self.folder = folder
+        self.url = url.strip()  # The YouTube URL to download
+        self.folder = folder  # The folder to download the video to
+        self.stream_type = stream_type  # The type of stream to download ('audio' or 'video')
 
+    # The code in this method is run when the thread is started
     def run(self):
         try:
+            # Create a YouTube object for the URL
             yt = YouTube(self.url)
-            audio = yt.streams.filter(only_audio=True).first()
-            if audio:
-                download_path = audio.download(self.folder)
-
-                # Create the mp3_path more efficiently
-                mp3_path = os.path.splitext(download_path)[0] + '.mp3'
-
-                clip = AudioFileClip(download_path)
-                clip.write_audiofile(mp3_path)
-                clip.close()  # Close the clip to free resources
-
-                if os.path.exists(download_path):
-                    os.remove(download_path)
+            
+            # Choose the first stream of the correct type (audio or video)
+            if self.stream_type == 'audio':
+                stream = yt.streams.filter(only_audio=True).first()
             else:
-                print(f"No audio stream found for URL: {self.url}")
+                stream = yt.streams.filter(progressive=True).first()
 
+            # Download the stream and convert it to MP3 format if it's an audio stream
+            if stream:
+                download_path = stream.download(self.folder)
+                if self.stream_type == 'audio':
+                    mp3_path = os.path.splitext(download_path)[0] + '.mp3'
+                    clip = AudioFileClip(download_path)
+                    clip.write_audiofile(mp3_path)
+                    clip.close()
+                    if os.path.exists(download_path):
+                        os.remove(download_path)
+            else:
+                print(f"No {self.stream_type} stream found for URL: {self.url}")
+
+        # Handle PytubeError exceptions
         except exceptions.PytubeError as e:
             print(f"PytubeError encountered: {str(e)}")
 
+# The Downloader class is the main application window
 class Downloader(QWidget):
     def __init__(self):
         super().__init__()
 
+        # Create the UI elements
         self.url_label = QLabel("Video URL:")
         self.url_edit = QLineEdit()
         self.folder_label = QLabel("Save to:")
@@ -46,6 +58,7 @@ class Downloader(QWidget):
         self.browse_button = QPushButton("Browse")
         self.download_button = QPushButton("Download")
 
+        # Arrange the UI elements in a vertical layout
         layout = QVBoxLayout()
         layout.addWidget(self.url_label)
         layout.addWidget(self.url_edit)
@@ -55,25 +68,32 @@ class Downloader(QWidget):
         layout.addWidget(self.download_button)
         self.setLayout(layout)
 
+        # Connect the buttons to their respective methods
         self.browse_button.clicked.connect(self.browse_folder)
         self.download_button.clicked.connect(self.start_download)
 
+        # Set the window title and size
         self.setWindowTitle("Video Downloader")
         self.resize(500, 200)
 
+    # Open a file dialog to choose the download folder
     def browse_folder(self):
         folder = QFileDialog.getExistingDirectory(self, "Choose Save Folder")
         self.folder_edit.setText(folder)
 
+    # Start two download threads (audio and video) when the Download button is clicked
     def start_download(self):
         url = self.url_edit.text()
         folder = self.folder_edit.text()
         if not url or not folder:
             return
 
-        self.download_thread = DownloadThread(url, folder)
-        self.download_thread.start()
+        self.audio_download_thread = DownloadThread(url, folder, 'audio')
+        self.video_download_thread = DownloadThread(url, folder, 'video')
+        self.audio_download_thread.start()
+        self.video_download_thread.start()
 
+# If this script is run directly (instead of imported), create and show the application window
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     downloader = Downloader()
